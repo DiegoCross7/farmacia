796486ee04ae800d2ddae2bd3b9c3db0829a2169
Diego Gabriel Centeno Falcon <diegogabrielcentenofalcon7@gmail.com>
2025-05-07 19:52:51 -0500

    Arreglo Factura impresion

--- Order.php
+++ Order.php
@@ -1,6 +1,5 @@
 <?php include('./constant/layout/head.php'); ?>
 <?php include('./constant/layout/header.php'); ?>
-
 <?php include('./constant/layout/sidebar.php'); ?>
 
 <style>
@@ -261,15 +260,18 @@
     }
 </style>
 
-<?php include('./constant/connect');
+<?php
+// 1) Incluye bien la conexión:
+include('./constant/connect.php');
+// 2) Inicializa el contador
+$no = 0;
 $user = $_SESSION['userId'];
-$sql = "SELECT  uno, orderDate, clientName, clientContact,paymentStatus,id FROM orders WHERE delete_status = 0";
+$sql = "SELECT uno, orderDate, clientName, clientContact, paymentStatus, id
+        FROM orders
+        WHERE delete_status = 0";
 $result = $connect->query($sql);
-
-//echo $sql;exit;
-
-//echo $itemCountRow;exit; 
 ?>
+
 <div class="page-wrapper">
 
     <div class="row page-titles">
@@ -309,49 +311,33 @@ $result = $connect->query($sql);
                             </tr>
                         </thead>
                         <tbody>
-                            <?php
-                            foreach ($result as $row) {
-
-                                $no += 1;
-                            ?>
+                            <?php foreach ($result as $row): $no++; ?>
                                 <tr>
-                                    <td class="text-center"><?= $no; ?></td>
-                                    <td><?php echo $row['orderDate'] ?></td>
-                                    <td><?php echo $row['clientName'] ?></td>
-                                    <td><?php echo $row['clientContact'] ?></td>
-
-
-                                    <td><?php if ($row['paymentStatus'] == 1) {
-
-                                            $paymentStatus = "<label class='label label-success' ><h4>Pago Completo</h4></label>";
-                                            echo $paymentStatus;
-                                        } else if ($row['payment_status'] == 2) {
-                                            $paymentStatus = "<label class='label label-danger'><h4>Pago Pactial</h4></label>";
-                                            echo $paymentStatus;
-                                        } else {
-                                            $paymentStatus = "<label class='label label-warning'><h4>Pago Pendiente</h4></label>";
-                                            echo $paymentStatus;
-                                        } // /els
-                                        ?></td>
+                                    <td class="text-center"><?= $no ?></td>
+                                    <td><?= $row['orderDate'] ?></td>
+                                    <td><?= $row['clientName'] ?></td>
+                                    <td><?= $row['clientContact'] ?></td>
                                     <td>
-
-                                        <a href="editorder.php?id=<?php echo $row['id'] ?>"><button type="button" class="btn btn-xs btn-primary"><i class="fa fa-pencil"></i></button></a>
-
-
-
-                                        <a href="php_action/removeOrder.php?id=<?php echo $row['id'] ?>"><button type="button" class="btn btn-xs btn-danger" onclick="return confirm('Deseas eliminar este registro?')"><i class="fa fa-trash"></i></button></a>
-
-                                        <a href="invoiceprint.php?id=<?php echo $row['id'] ?>"><button type="button" class="btn btn-xs btn-success"><i class="fa fa-print"></i></button></a>
-
-
+                                        <?php
+                                        if ($row['paymentStatus'] == 1)      echo "<label class='label label-success'><h4>Pago Completo</h4></label>";
+                                        elseif ($row['paymentStatus'] == 2)  echo "<label class='label label-danger'><h4>Pago Parcial</h4></label>";
+                                        else                                 echo "<label class='label label-warning'><h4>Pago Pendiente</h4></label>";
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <a href="editorder.php?id=<?= $row['id'] ?>">
+                                            <button class="btn btn-xs btn-primary"><i class="fa fa-pencil"></i></button>
+                                        </a>
+                                        <a href="php_action/removeOrder.php?id=<?= $row['id'] ?>" onclick="return confirm('Deseas eliminar este registro?')">
+                                            <button class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></button>
+                                        </a>
+                                        <a href="invoiceprint.php?id=<?= $row['id'] ?>">
+                                            <button class="btn btn-xs btn-success"><i class="fa fa-print"></i></button>
+                                        </a>
                                     </td>
                                 </tr>
-
+                            <?php endforeach; ?>
                         </tbody>
-                    <?php
-                            }
-
-                    ?>
                     </table>
                 </div>
             </div>
--- invoiceprint.php
+++ invoiceprint.php
@@ -1,323 +1,227 @@
+<?php
+require_once 'constant/connect.php';
+
+// 1) Validar GET
+if (empty($_GET['id']) || !is_numeric($_GET['id'])) {
+    die('Error: Falta el parámetro id de la factura');
+}
+$id = intval($_GET['id']);
+
+// 2) Recuperar la orden (solo un registro)
+$sql  = "SELECT * FROM orders WHERE delete_status = 0 AND id = $id";
+$res  = $connect->query($sql);
+if (!$res || $res->num_rows === 0) {
+    die('Factura no encontrada');
+}
+$order = $res->fetch_assoc();
+
+// 3) Recuperar los items
+$sql2  = "SELECT oi.*, p.product_name, p.bno, p.expdate, p.mrp
+          FROM order_item AS oi
+          JOIN product AS p ON p.product_id = oi.productName
+          WHERE oi.lastid = $id";
+$res2  = $connect->query($sql2);
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
-	<meta charset="utf-8" />
-	<title>Invoice Print</title>
+    <meta charset="utf-8" />
+    <title>Invoice Print</title>
 
-	<style>
-		.invoice-box {
-			max-width: 800px;
-			margin: auto;
-			padding: 30px;
-			border: 1px solid #eee;
-			box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-			font-size: 16px;
-			line-height: 24px;
-			font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-			color: #555;
-		}
+    <style>
+        .invoice-box {
+            max-width: 800px;
+            margin: auto;
+            padding: 30px;
+            border: 1px solid #eee;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+            font-size: 16px;
+            line-height: 24px;
+            font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+            color: #555;
+        }
 
-		.invoice-box table {
-			width: 100%;
-			line-height: inherit;
-			text-align: left;
-			border-collapse: collapse;
-		}
+        .invoice-box table {
+            width: 100%;
+            line-height: inherit;
+            text-align: left;
+            border-collapse: collapse;
+        }
 
-		.invoice-box table td {
-			padding: 5px;
-			vertical-align: top;
-		}
+        .invoice-box table td {
+            padding: 5px;
+            vertical-align: top;
+        }
 
-		.invoice-box table tr td:nth-child(2) {
-			text-align: right;
-		}
+        .invoice-box table tr td:nth-child(2) {
+            text-align: right;
+        }
 
-		.invoice-box table tr.top table td {
-			padding-bottom: 20px;
-		}
+        .invoice-box table tr.top table td {
+            padding-bottom: 20px;
+        }
 
-		.invoice-box table tr.top table td.title {
-			font-size: 45px;
-			line-height: 45px;
-			color: #333;
-		}
+        .invoice-box table tr.top table td.title {
+            font-size: 45px;
+            line-height: 45px;
+            color: #333;
+        }
 
-		.invoice-box table tr.information table td {
-			padding-bottom: 40px;
-		}
+        .invoice-box table tr.information table td {
+            padding-bottom: 40px;
+        }
 
-		.invoice-box table tr.heading td {
-			background: #eee;
-			border-bottom: 1px solid #ddd;
-			font-weight: bold;
-		}
+        .invoice-box table tr.heading td {
+            background: #eee;
+            border-bottom: 1px solid #ddd;
+            font-weight: bold;
+        }
 
-		.invoice-box table tr.details td {
-			padding-bottom: 20px;
-		}
+        .invoice-box table tr.details td {
+            padding-bottom: 20px;
+        }
 
-		.invoice-box table tr.item td {
-			/*border-bottom: 1px solid #eee;*/
-		}
+        .invoice-box table tr.item td {
+            /*border-bottom: 1px solid #eee;*/
+        }
 
-		.invoice-box table tr.item.last td {
-			border-bottom: none;
-		}
+        .invoice-box table tr.item.last td {
+            border-bottom: none;
+        }
 
-		.invoice-box table tr.total td:nth-child(2) {
-			/*border-top: 2px solid #eee;*/
-			font-weight: bold;
-		}
+        .invoice-box table tr.total td:nth-child(2) {
+            /*border-top: 2px solid #eee;*/
+            font-weight: bold;
+        }
 
-		@media only screen and (max-width: 600px) {
-			.invoice-box table tr.top table td {
-				width: 100%;
-				display: block;
-				text-align: center;
-			}
+        @media only screen and (max-width: 600px) {
+            .invoice-box table tr.top table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
 
-			.invoice-box table tr.information table td {
-				width: 100%;
-				display: block;
-				text-align: center;
-			}
-		}
+            .invoice-box table tr.information table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
+        }
 
-		/** RTL **/
-		.invoice-box.rtl {
-			direction: rtl;
-			font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-		}
+        /** RTL **/
+        .invoice-box.rtl {
+            direction: rtl;
+            font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+        }
 
-		.invoice-box.rtl table {
-			text-align: right;
-		}
+        .invoice-box.rtl table {
+            text-align: right;
+        }
 
-		.invoice-box.rtl table tr td:nth-child(2) {
-			text-align: left;
-		}
-	</style>
+        .invoice-box.rtl table tr td:nth-child(2) {
+            text-align: left;
+        }
+    </style>
 </head>
-<?php
 
-require_once('constant/connect.php');
-$sql = "SELECT  * FROM orders WHERE delete_status = 0 and id='" . $_GET['id'] . "' ";
-//echo $sql;exit;
-$result = $connect->query($sql);
-foreach ($result as $row) { //print_r($row);exit;
+<body>
+    <div class="invoice-box">
+        <table cellpadding="0" cellspacing="0">
+            <tr class="top">
+                <td colspan="7">
+                    <table>
+                        <tr>
+                            <td class="title">
+                                <img src="./assets/uploadImage/Logo/logo_factura.jpg" style="max-width:300px" />
+                            </td>
 
-?>
+                            <td>
+                                Factura #: <?= htmlspecialchars($order['uno']) ?><br>
+                                Fecha: <?= htmlspecialchars($order['orderDate']) ?>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
 
-	<body>
-		<div class="invoice-box">
-			<table cellpadding="0" cellspacing="0">
-				<tr class="top">
-					<td colspan="6">
-						<table>
-							<tr>
-								<td class="title">
-									<img src="./assets/uploadImage/Logo/logo_factura.jpg" style="width: 100%; max-width: 300px" />
-								</td>
+            <tr class="information">
+                <td colspan="7">
+                    <table>
+                        <tr>
+                            <td>
+                                <?= htmlspecialchars($order['clientName']) ?><br>
+                                <?= htmlspecialchars($order['clientContact']) ?><br>
+                                <?= htmlspecialchars($order['address']) ?>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
 
-								<td>
-									<h1 style="margin:0 0 11px;">Factura</h1> #:<?php echo $row['uno'] ?><br />
-									Fecha:<?php echo $row['orderDate'] ?><br />
+            <tr class="heading">
+                <td>Método de Pago</td>
+                <td>Monto</td>
+            </tr>
+            <tr class="details">
+                <td>
+                    <?php
+                    // Mostrar el método de pago basado en el campo paymentType
+                    switch ($order['paymentType']) {
+                        case 1:
+                            echo "Efectivo";
+                            break;
+                        case 2:
+                            echo "Tarjeta";
+                            break;
+                        case 3:
+                            echo "Transferencia";
+                            break;
+                        default:
+                            echo "Otro";
+                            break;
+                    }
+                    ?>
+                </td>
+                <td><?= htmlspecialchars($order['paid']) ?></td>
+            </tr>
 
-								</td>
+            <tr class="heading">
+                <td>Medicina</td>
+                <td>Lote</td>
+                <td>Exp</td>
+                <td>Qty</td>
+                <td>MRP</td>
+                <td>CxP</td>
+                <td>Total</td>
+            </tr>
 
-							</tr>
-						</table>
-					</td>
-				</tr>
+            <?php while ($item = $res2->fetch_assoc()) : ?>
+                <tr class="item">
+                    <td><?= htmlspecialchars($item['product_name']) ?></td>
+                    <td><?= htmlspecialchars($item['bno']) ?></td>
+                    <td><?= htmlspecialchars($item['expdate']) ?></td>
+                    <td><?= htmlspecialchars($item['quantity']) ?></td>
+                    <td><?= htmlspecialchars($item['mrp']) ?></td>
+                    <td><?= htmlspecialchars($item['rate']) ?></td>
+                    <td><?= htmlspecialchars($item['total']) ?></td>
+                </tr>
+            <?php endwhile; ?>
 
-				<tr class="information">
-					<td colspan="2">
-
-						<table>
-							<tr><?php
-								require_once('constant/connect.php');
-								$sql1 = "SELECT email,address,user_id FROM users ";
-								//echo $sql;exit;
-								$result1 = $connect->query($sql1);
-								//echo $result1;exit;
-								foreach ($result1 as $row1) { ?>
-									<td>
-										<?php echo $row1['email'] ?><br />
-										<?php echo $row1['address'] ?><br />
-
-									</td>
-								<?php } ?>
-								<td>
-									<?php echo $row['clientName'] ?>
-									<br />
-									<?php echo $row['clientContact'] ?>
-									<br />
-									<?php echo $row['address'] ?>
-								</td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-
-				<tr class="heading">
-					<td colspan="3">Método de Pago</td>
-
-					<td colspan="4">Monto</td>
-				</tr>
-
-				<tr class="details">
-					<td colspan="3"><?php if ($row['paymentType'] == 4) {
-										echo "Nequi";
-									}
-									if ($row['paymentType'] == 5) {
-										echo "Google Pay";
-									}
-									if ($row['paymentType'] == 3) {
-										echo "Tarjeta de Crédito";
-									}
-									if ($row['paymentType'] == 2) {
-										echo "Efectivo";
-									}
-									if ($row['paymentType'] == 1) {
-										echo "Cheque";
-									} ?></td>
-
-					<td colspan="4"><?php echo $row['grandTotalValue'] ?></td>
-
-				</tr>
-
-				<tr class="heading">
-					<td>Medicina</td>
-					<td>Lote</td>
-					<td>Exp</td>
-					<td>Qty</td>
-					<td>MRP</td>
-					<td>CxP</td>
-					<td>Total</td>
-
-
-				</tr>
-				<?php
-				require_once('constant/connect.php');
-				$sql2 = "SELECT productName,total,rate,quantity FROM order_item WHERE lastid='" . $_GET['id'] . "' ";
-				//echo $sql2;exit;
-				$result2 = $connect->query($sql2);
-				//echo $result2;exit;
-				foreach ($result2 as $row2) { //print_r($row2);exit; 
-				?>
-					<tr class="item">
-						<td><?php
-							//echo "SELECT * FROM product WHERE product_id='".$row2['productName']."'";exit;
-							$stmt1 = "SELECT * FROM product WHERE product_id='" . $row2['productName'] . "'";
-							$result2 = $connect->query($stmt1);
-
-							foreach ($result2 as $key1)
-							?>
-
-							<?php echo $key1['product_name']; ?></td>
-						<td><?php echo $key1['bno'] ?></td>
-						<td><?php echo $key1['expdate'] ?></td>
-						<td><?php echo $row2['quantity'] ?></td>
-						<td><?php echo $key1['mrp'] ?></td>
-						<td><?php echo $row2['rate'] ?></td>
-
-						<td><?php echo $row2['total']; ?></td>
-					</tr>
-					<tr>
-						<td rowspan="3" colspan="3" style="border: 1px solid #555">A/C NO:0064867230047 <br>SWIFT:DTDA1320
-							<br>Banco del Pueblo
-						</td>
-						<td colspan="3" style="border: 1px solid;text-align: left;">Descuentos</td>
-						<?php
-
-						$stmt1 = "SELECT * FROM orders WHERE clientName='" . $row['clientName'] . "'";
-						$cola = $connect->query($stmt1);
-
-						foreach ($cola as $pepsi)
-						?>
-						<td style="border: 1px solid"><?php echo $pepsi['discount'] ?></td>
-					</tr>
-					<tr>
-						<td colspan="3" style="border: 1px solid">Impuestos</td>
-						<td style="border: 1px solid;text-align: left;"><?php echo $pepsi['gstn'] ?></td>
-					</tr>
-					<tr>
-						<td colspan="3" style="border: 1px solid">Total</td>
-						<td style="border: 1px solid;text-align: left;"><?php echo $pepsi['grandTotalValue'] ?></td>
-					</tr>
-
-				<?php $totalAmount += $pepsi['grandTotalValue'];
-				} ?>
-
-				<!-- <tr class="item">
-					<td>Hosting (3 months)</td>
-
-					<td>$75.00</td>
-				</tr>
-
-				<tr class="item last">
-					<td>Domain name (1 year)</td>
-
-					<td>$10.00</td>
-				</tr>
- -->
-				<tr class="total">
-
-
-					<?php
-
-					$number = $totalAmount;
-					$no = floor($number);
-					$point = round($number - $no, 2) * 100;
-					$hundred = null;
-					$digits_1 = strlen($no);
-					$i = 0;
-					$str = array();
-					$words = array(
-						'0' => '', '1' => 'One', '2' => 'Two',
-						'3' => 'Three', '4' => 'Four', '5' => 'Five', '6' => 'Six',
-						'7' => 'Seven', '8' => 'Eight', '9' => 'Nine',
-						'10' => 'Ten', '11' => 'Eleven', '12' => 'Twelve',
-						'13' => 'Thirteen', '14' => 'Fourteen',
-						'15' => 'Fifteen', '16' => 'Sixteen', '17' => 'Seventeen',
-						'18' => 'Eighteen', '19' => 'Nineteen', '20' => 'Twenty',
-						'30' => 'Thirty', '40' => 'Forty', '50' => 'Fifty',
-						'60' => 'Sixty', '70' => 'Seventy',
-						'80' => 'Eighty', '90' => 'Ninety'
-					);
-					$digits = array('', 'Hundred', 'Thousand', 'Lakh', 'Crore');
-					while ($i < $digits_1) {
-						$divider = ($i == 2) ? 10 : 100;
-						$number = floor($no % $divider);
-						$no = floor($no / $divider);
-						$i += ($divider == 10) ? 1 : 2;
-						if ($number) {
-							$plural = (($counter = count($str)) && $number > 9) ? 's' : null;
-							$hundred = ($counter == 1 && $str[0]) ? ' and ' : null;
-							$str[] = ($number < 21) ? $words[$number] .
-								" " . $digits[$counter] . $plural . " " . $hundred
-								:
-								$words[floor($number / 10) * 10]
-								. " " . $words[$number % 10] . " "
-								. $digits[$counter] . $plural . " " . $hundred;
-						} else $str[] = null;
-					}
-					$str = array_reverse($str);
-					$result = implode('', $str);
-					$points = ($point) ?
-						"." . $words[$point / 10] . " " .
-						$words[$point = $point % 10] : '';
-
-					?>
-					<td>Monto:<b><?php echo $result . "COP  " . $points; ?></b></td>
-					<td>Total: <?php echo $totalAmount; ?> </td>
-				</tr>
-			</table>
-
-		</div>
-	</body>
-<?php } ?>
+            <tr class="total">
+                <td colspan="6" style="text-align: right;">Descuentos:</td>
+                <td><?= htmlspecialchars($order['discount']) ?></td>
+            </tr>
+            <tr class="total">
+                <td colspan="6" style="text-align: right;">Impuestos:</td>
+                <td><?= htmlspecialchars($order['gstn']) ?></td>
+            </tr>
+            <tr class="total">
+                <td colspan="6" style="text-align: right;">Total:</td>
+                <td><?= htmlspecialchars($order['grandTotalValue']) ?></td>
+            </tr>
+        </table>
+    </div>
+</body>
 
 </html>
